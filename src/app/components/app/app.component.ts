044e626769a6dbf223922054fbe8832eee50b190
Andrei Vial <andr.vyal@gmail.com>
2022-05-09 11:46:23 +0300

    App prefix

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gst-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'giphy-search-test';
 }
